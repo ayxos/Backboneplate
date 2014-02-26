0bb76c87ad1dd4f1d60f66b572f1df113ece62be
cybermarkus1 <cybermarkus1@gmail.com>
2014-02-26 16:39:38 +0100

    removing extra files AND finish struct

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var express = require('express'),
   path  = require('path'),
-  fs    = require('fs'),
   http = require('http'),
-  uuid    = require('node-uuid');
+  routes = require('./routes'); // Para las funciones con la DB
 
-var app = express(),
-  baseDeDatos = fs.readFileSync('./routes/datos.json').toString();
-
-var datos = JSON.parse(baseDeDatos);
+var app = express();
 
 app.set('port', process.env.PORT || 8001);
 app.set('views', path.join(__dirname, 'views'));
@@ -26,84 +22,25 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// Pagina de Inicio: http:localhost:4000
-app.get('/', function (req,res){
-  res.render( 'index', {
-    title : 'Backbone boilerplate System with Node/Express',
-    footer : '@2014 by M.A.P.S Powered by Node.js, Express, Backbone '
-  });
-});
+// Pagina de Inicio: http:localhost:PORT
+app.get('/', routes.Main );
 
 // API REST
 
 // Mostrar todos los libros
-app.get('/libros', function(req,res) {
-  res.send(datos);
-});
+app.get('/libros', routes.ShowAll );
 
 // Mostrar el detalle de un libro
-app.get('/libros/:id', function(req,res,next) {
-  var dato;
-
-  for ( var i=0; i<datos.length; i++ ) {
-    var libro = datos[i];
-
-    if (libro.id === req.params.id) {
-      dato = libro;
-    }
-  }
-
-  if (dato) {
-    res.send(dato);
-  } else {
-    res.statusCode = 500;
-    return res.send('No se encuentra el id.');
-  }
-
-});
+app.get('/libros/:id', routes.getById );
 
 // POST: crear un nuevo libro.
-app.post('/libros', function (req, res){
-  req.body.id = uuid.v1(4);
-
-  datos.push(req.body);
-
-  res.send(200, {id: req.body.id});
-});
+app.post('/libros', routes.createNew );
 
 // PUT: Actualizar un libro.
-app.put('/libros/:id', function (req, res){
-  var libro;
-
-  for (var i = datos.length - 1; i >= 0; i--) {
-    libro = datos[i];
-
-    if(libro.id === req.params.id){
-      datos[i] = req.body;
-    }
-  }
-
-  res.send(200);
-});
+app.put('/libros/:id', routes.update );
 
 // DELETE: Eliminar un libro.
-app.delete('/libros/:id', function (req,res) {
-
-  var elementoEliminar;
-
-  for ( var i=0; i<datos.length; i++ ) {
-    var libro = datos[i];
-
-    if (libro.id === req.params.id) {
-      elementoEliminar = i;
-    }
-  }
-
-  datos.splice(elementoEliminar, 1);
-
-  res.send(200);
-
-});
+app.delete('/libros/:id', routes.del );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
